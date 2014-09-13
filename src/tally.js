9713eb74bb6683b3c9c9716d0d0ac21668eb3d51
Tim Dettrick <tim@dettrick.net>
2014-09-13 14:55:28 +0000

    Adding more tests, and a structure for FPTP.

--- src/tally.js
+++ src/tally.js
@@ -1,9 +1,53 @@
 (function(done) {
   
+  var utils = {};
+  utils.count = function(items) {
+    // TODO: return count for each item
+    // ie. count(["a","b","b", null]) = { null: 1, "a": 1, "b": 2 }
+    return {};
+  }
+  
+  // Voting Methods
+  var methods = {};
+  
+  methods.FPTP = (function IIFE() {
+    // Remove invalid votes, and turn valid votes into single candidate
+  	var validator = function(candidates) {
+      return function() {
+        // TODO: return candidate, or null if invalid
+        return null;
+      }
+    };
+    
+    var method = function(options, candidates, votes) {
+      // Compact and count votes
+      var tally = utils.count(votes.map( validator(candidates) ));
+      // TODO: Declate highest non-null candidate from tally the winner
+      return {};
+    };
+    
+    // Export for testing
+    method.validator = validator;
+    
+    return method;
+  })();
+  
+  // Main Function
   var tallyFunc = function(election) {
-    // TODO: return something other than the input!
-    return election;
+    var method = methods[election.type];
+    if (!method) {
+      throw new Error("Unknown election type.");
+    }
+    // Pass to method - for starters we're forgiving about required parameters
+    return method(
+      election.options || {},
+      election.candidates || [],
+      election.votes || []);
   };
+  
+  // Exports components for testing
+  tallyFunc.utils = utils;
+  tallyFunc.methods = methods;
   
   // Export tally function
   done(tallyFunc);
--- test/tally_spec.js
+++ test/tally_spec.js
@@ -29,11 +29,58 @@ describe('#tally()', function() {
       });
     });
     
+    it('should throw an error if invalid election type', function(done) {
+      fromFile('fptp/valid_01.json', function(input) {
+        input.type = "invalid_election_type";
+        expect(function() { tally(input) }).to.throw(Error);
+        done();
+      });
+    });
+    
   });
   
   describe('First Past The Post', function() {
     
     it('should yield correct result in elections with invalid votes');
+    
+  });
+  
+});
+
+describe('#tally.utils', function() {
+  var utils = require('../src/tally').utils;
+  
+  describe('.count', function() {
+  	var count = utils.count;
+    
+    // TODO: Skipping - make this pass!
+    it.skip('should count instances of each item', function(done) {
+      expect(count([])).to.deep.equal({});
+      expect(count(['a'])).to.deep.equal({ "a": 1 });
+      expect(count(['a', 'b', 'b'])).to.deep.equal({ "a": 1, "b": 2 });
+      done();
+    });
+    
+  });
+  
+});
+
+describe('#tally.methods.fptp', function() {
+  var FPTP = require('../src/tally').methods.FPTP;
+  
+  describe('.validator', function() {
+    var validator = FPTP.validator;
+    
+    it('should yield a function from a list of candidates', function(done) {
+      expect(validator(['a', 'b'])).to.be.a('function');
+      done();
+    });
+    
+    it('should yield null if not a single candidate');
+    
+    it('should yield null if not a given candidate');
+    
+    it('should yield candidate if valid vote');
     
   });
   
